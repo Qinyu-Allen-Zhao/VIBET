a5141e59cd2580eba0b62c0da72f60e287728c7c
Qinyu Zhao <qinyu.zhao@anu.edu.au>
2022-10-04 20:54:06 +1100

    Change the preparing script

--- scripts/prepare_training_data.sh
+++ scripts/prepare_training_data.sh
@@ -6,21 +6,21 @@ export PYTHONPATH="./:$PYTHONPATH"
 # AMASS
 python lib/data_utils/amass_utils.py --dir ./data/amass
 
-# InstaVariety
-# Comment this if you already downloaded the preprocessed file
-python lib/data_utils/insta_utils.py --dir ./data/insta_variety
-
-# 3DPW
-python lib/data_utils/threedpw_utils.py --dir ./data/3dpw
-
-# MPI-INF-3D-HP
-python lib/data_utils/mpii3d_utils.py --dir ./data/mpi_inf_3dhp
-
-# PoseTrack
-python lib/data_utils/posetrack_utils.py --dir ./data/posetrack
-
-# PennAction
-python lib/data_utils/penn_action_utils.py --dir ./data/penn_action
+## InstaVariety
+## Comment this if you already downloaded the preprocessed file
+#python lib/data_utils/insta_utils.py --dir ./data/insta_variety
+#
+## 3DPW
+#python lib/data_utils/threedpw_utils.py --dir ./data/3dpw
+#
+## MPI-INF-3D-HP
+#python lib/data_utils/mpii3d_utils.py --dir ./data/mpi_inf_3dhp
+#
+## PoseTrack
+#python lib/data_utils/posetrack_utils.py --dir ./data/posetrack
+#
+## PennAction
+#python lib/data_utils/penn_action_utils.py --dir ./data/penn_action
 
 # Synthesis videos
 python lib/data_synthesis/syn_videos_process.py --dir ./data/syn_videos
