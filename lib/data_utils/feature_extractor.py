5fb41ea41c23fdcd1ee5a30ff65937dfeb68ed5e
Qinyu Zhao <qinyu.zhao@anu.edu.au>
2022-10-06 13:42:46 +1100

    Random mask.

--- lib/data_utils/feature_extractor.py
+++ lib/data_utils/feature_extractor.py
@@ -13,10 +13,11 @@
 # for Intelligent Systems. All rights reserved.
 #
 # Contact: ps-license@tuebingen.mpg.de
-
+import math
 import os
 import torch
 import torchvision
+import torchvision.transforms.functional as F
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -60,9 +61,9 @@ def extract_features(model, video, bbox, debug=False, batch_size=200,
         for i in range(kp_2d.shape[1]):
             frame = cv2.circle(
                 frame.copy(),
-                (int(kp_2d[0,i,0]), int(kp_2d[0,i,1])),
+                (int(kp_2d[0, i, 0]), int(kp_2d[0, i, 1])),
                 thickness=3,
-                color=(255,0,0),
+                color=(255, 0, 0),
                 radius=3,
             )
 
@@ -86,6 +87,8 @@ def extract_features(model, video, bbox, debug=False, batch_size=200,
 
     with torch.no_grad():
         for images in frames:
+            if random_mask:
+                create_random_mask(images)
 
             if not debug:
                 pred = model.feature_extractor(images)
@@ -113,3 +116,42 @@ def extract_features(model, video, bbox, debug=False, batch_size=200,
         features = torch.cat(features, dim=0)
 
     return features.numpy()
+
+
+def create_random_mask(images, scale=(0.02, 0.2), ratio=(0.3, 3.3), value=0):
+    img = images[0]
+
+    i, j, h, w, v = random_mask_for_one_image(img, ratio, scale, value)
+
+    for n, img in enumerate(images):
+        images[n] = F.erase(img, i, j, h, w, v)
+
+    # Return original image
+    return images
+
+
+def random_mask_for_one_image(img, ratio, scale, value):
+    # Randomly create one mask and apply it to all frames in a video
+    img_c, img_h, img_w = img.shape[-3], img.shape[-2], img.shape[-1]
+    area = img_h * img_w
+    log_ratio = torch.log(torch.tensor(ratio))
+    for _ in range(10):
+        erase_area = area * torch.empty(1).uniform_(scale[0], scale[1]).item()
+        aspect_ratio = torch.exp(torch.empty(1).uniform_(log_ratio[0], log_ratio[1])).item()
+
+        h = int(round(math.sqrt(erase_area * aspect_ratio)))
+        w = int(round(math.sqrt(erase_area / aspect_ratio)))
+        if not (h < img_h and w < img_w):
+            continue
+
+        if value is None:
+            v = torch.empty([img_c, h, w], dtype=torch.float32).normal_()
+        else:
+            v = torch.tensor(value)[:, None, None]
+
+        i = torch.randint(0, img_h - h + 1, size=(1,)).item()
+        j = torch.randint(0, img_w - w + 1, size=(1,)).item()
+        return i, j, h, w, v
+
+    # Return original image
+    return 0, 0, img_h, img_w, img
