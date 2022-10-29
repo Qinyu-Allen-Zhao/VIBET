cd39fd1c89b1af3fad2d29c364d417e0eff523cb
Qinyu Zhao <qinyu.zhao@anu.edu.au>
2022-10-29 21:30:54 +1100

    Modify script

--- scripts/prepare_training_data.sh
+++ scripts/prepare_training_data.sh
@@ -4,23 +4,23 @@ mkdir -p ./data/vibe_db
 export PYTHONPATH="./:$PYTHONPATH"
 
 ## AMASS
-#python lib/data_utils/amass_utils.py --dir ./data/amass
+python lib/data_utils/amass_utils.py --dir ./data/amass
 
 ## InstaVariety
 ## Comment this if you already downloaded the preprocessed file
 #python lib/data_utils/insta_utils.py --dir ./data/insta_variety
-#
+
 ## 3DPW
-#python lib/data_utils/threedpw_utils.py --dir ./data/3dpw
+python lib/data_utils/threedpw_utils.py --dir ./data/3dpw
 #
 ## MPI-INF-3D-HP
-#python lib/data_utils/mpii3d_utils.py --dir ./data/mpi_inf_3dhp
+python lib/data_utils/mpii3d_utils.py --dir ./data/mpi_inf_3dhp
 #
 ## PoseTrack
-#python lib/data_utils/posetrack_utils.py --dir ./data/posetrack
+python lib/data_utils/posetrack_utils.py --dir ./data/posetrack
 #
 ## PennAction
-#python lib/data_utils/penn_action_utils.py --dir ./data/penn_action
+python lib/data_utils/penn_action_utils.py --dir ./data/penn_action
 
 # Synthesis videos
 python lib/data_synthesis/syn_videos_process.py --dir ./data/syn_videos
