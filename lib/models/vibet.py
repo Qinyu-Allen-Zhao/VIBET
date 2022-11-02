e957be02507bda3afea79ed7e70ae8832b5f3cf2
Qinyu Zhao <qinyu.zhao@anu.edu.au>
2022-11-02 21:27:47 +1100

    Modify the architecture of transformer

--- lib/models/vibet.py
+++ lib/models/vibet.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import math
 import os
 import torch
 import os.path as osp
@@ -7,6 +7,24 @@ import torch.nn as nn
 
 from lib.core.config import VIBE_DATA_DIR
 from lib.models.spin import Regressor, hmr
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + self.pe[:x.size()[0], :]
+        return self.dropout(x)
 
 
 class TemporalEncoder(nn.Module):
@@ -17,14 +35,18 @@ class TemporalEncoder(nn.Module):
             num_layers=6
     ):
         super(TemporalEncoder, self).__init__()
+        self.pos_encoder = PositionalEncoding(d_model, dropout=0.1)
         self.encoder_layer = nn.TransformerEncoderLayer(d_model=d_model, nhead=nhead)
         self.transformer_encoder = nn.TransformerEncoder(
             self.encoder_layer,
             num_layers=num_layers
         )
+        self.decoder = nn.Linear(d_model, 2048)
 
     def forward(self, x):
+        x = self.pos_encoder(x)
         out = self.transformer_encoder(x)
+        out = self.decoder(out)
         return out
 
 
