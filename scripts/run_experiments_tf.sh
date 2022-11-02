093ece478776894f1f6b56ebf347287b6d0d2c2b
Qinyu Zhao <qinyu.zhao@anu.edu.au>
2022-11-02 21:34:15 +1100

    Modify the experiments of transformer

--- scripts/run_experiments_tf.sh
+++ scripts/run_experiments_tf.sh
@@ -1,7 +1,8 @@
-python train.py --cfg experiments/transformer/train_with_tf_4H_2L.yaml
-
-python train.py --cfg experiments/transformer/train_with_tf_8H_4L.yaml
-
-python train.py --cfg experiments/transformer/train_with_tf_12H_8L.yaml
-
-python train.py --cfg experiments/transformer/train_with_tf_16H_12L.yaml
+python train.py --cfg experiments/transformer/train_with_tf_1H_6L.yaml
+python train.py --cfg experiments/transformer/train_with_tf_4H_6L.yaml
+python train.py --cfg experiments/transformer/train_with_tf_8H_6L.yaml
+python train.py --cfg experiments/transformer/train_with_tf_16H_6L.yaml
+python train.py --cfg experiments/transformer/eval_with_tf_1H_6L.yaml
+python train.py --cfg experiments/transformer/eval_with_tf_4H_6L.yaml
+python train.py --cfg experiments/transformer/eval_with_tf_8H_6L.yaml
+python train.py --cfg experiments/transformer/eval_with_tf_16H_6L.yaml
