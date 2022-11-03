4d720361dec6f5bc433b56d07878ac84c7661d55
Qinyu Zhao <qinyu.zhao@anu.edu.au>
2022-11-03 23:12:07 +1100

    Set the last night's experiments.

--- scripts/run_experiments_1103.sh
+++ scripts/run_experiments_1103.sh
@@ -1,3 +1,6 @@
+python train.py --cfg experiments/reproduce/config_w_3dpw.yaml
+python eval.py --cfg experiments/reproduce/evaluate_ori.yaml
+
 python train.py --cfg experiments/transformer/train_with_tf_simple.yaml
 python eval.py --cfg experiments/transformer/eval_with_tf_simple.yaml
 
