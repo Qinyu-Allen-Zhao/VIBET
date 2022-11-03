17653b92fe5fbe02e6572554ce80af73cc6afa32
Qinyu Zhao <qinyu.zhao@anu.edu.au>
2022-11-03 13:34:31 +1100

    Simple transformer models

--- scripts/run_experiments_tf.sh
+++ scripts/run_experiments_tf.sh
@@ -1,15 +1,19 @@
-# Trainsformer 1 nhead 6 layers
+# Transformer 1 nhead 6 layers
 #python train.py --cfg experiments/transformer/train_with_tf_1H_6L.yaml
 python eval.py --cfg experiments/transformer/eval_with_tf_1H_6L.yaml
 
-# Trainsformer 4 nheads 6 layers
+# Transformer 4 nheads 6 layers
 #python train.py --cfg experiments/transformer/train_with_tf_4H_6L.yaml
 python eval.py --cfg experiments/transformer/eval_with_tf_4H_6L.yaml
 
-# Trainsformer 8 nheads 6 layers
+# Transformer 8 nheads 6 layers
 #python train.py --cfg experiments/transformer/train_with_tf_8H_6L.yaml
 python eval.py --cfg experiments/transformer/eval_with_tf_8H_6L.yaml
 
-# Trainsformer 16 nheads 6 layers
+# Transformer 16 nheads 6 layers
 #python train.py --cfg experiments/transformer/train_with_tf_16H_6L.yaml
 python eval.py --cfg experiments/transformer/eval_with_tf_16H_6L.yaml
+
+# Transformer 8 nheads 1 layer
+python train.py --cfg experiments/transformer/train_with_tf_8H_1L.yaml
+python eval.py --cfg experiments/transformer/eval_with_tf_8H_1L.yaml
