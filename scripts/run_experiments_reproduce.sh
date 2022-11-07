9cad14f4d065d40b15bc6562717350d994828f19
Qinyu Zhao <qinyu.zhao@anu.edu.au>
2022-11-07 19:09:54 +1100

    Add much more comments and documentations

--- scripts/run_experiments_reproduce.sh
+++ scripts/run_experiments_reproduce.sh
@@ -1,5 +1,7 @@
+# Training the vibe model with 3DPW
 python train.py --cfg experiments/reproduce/config_w_3dpw.yaml
 python eval.py --cfg experiments/reproduce/evaluate_ori.yaml
 
+# Training the vibe model without 3DPW
 python train.py --cfg experiments/reproduce/config_wo_3dpw.yaml
 python eval.py --cfg experiments/reproduce/evaluate_ori_wo_3dpw.yaml
